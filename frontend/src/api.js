4d0d65d3ee378b441d513e490658fcd5adfbf8f5
Vinothini31 <vinothinim012@gmail.com>
2025-11-28 10:23:31 +0530

    Update api.js

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,5 +1,11 @@
 import axios from "axios";
 
+// Auto switch between local and production
+const baseURL =
+  window.location.hostname === "localhost"
+    ? "http://localhost:5000/api"
+    : "https://roomiconnect-fs-backend.onrender.com/api";
+
 export default axios.create({
-  baseURL: "http://localhost:5000/api"
+  baseURL,
 });
